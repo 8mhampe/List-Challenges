ff1216fbe57e00682e5a9a79cc567d32d70ec151
8mhampe <31480929+8mhampe@users.noreply.github.com>
2018-04-11 10:08:43 -0500

    Add files via upload

--- listTasks.js
+++ listTasks.js
@@ -1,3 +1,4 @@
+//Task 1
 function largest(list){
     let largestNumber = list[0];
     for(let n=1; n<list.length; n++){
@@ -10,21 +11,33 @@ function largest(list){
 
 //Task 2
 function reverse(list){
-    let newArray = [];
+    let temp = [];
     for(let r=0; r<list.length; r++){
-        newArray.push(list[r]);
+        temp.push(list[r]);
     }
-    return newArray;
+    return list;
 }
 
 //Task 3
 function contains(list,e){
-
+    let temp = [];
+    for(let c=0; c<list.length; c++){
+        if(list[c] == e){
+          temp.push(list[c]);
+        }
+    }
+    return temp;
 }
 
 //Task 4
 function oddElements(list){
-
+    let temp = [];
+    for(let d=1; d<list.length; d++){
+        if(d == isOdd()){
+          temp.push(list[d]);
+        }
+    }
+    return temp;
 }
 
 //Task 5
@@ -67,5 +80,12 @@ function rotate(list,k){
 function fibonacci(){
 
 }
-    let testVariable = new reverse([6,5,9,10,7,3])
+    let testVariable = new largest([8,5,2,3,6,9,1,4,7])
+    let testVariablei = new reverse([6,5,9,10,7,3])
+    let testVariableii = new contains([6,5,8,7,9,3,2,1],5)
+    let testVariableiii = new reverse([6,5,9,10,7,3])
+
     console.log(testVariable);
+    console.log(testVariablei);
+    console.log(testVariableii);
+    console.log(testVariableiii);
